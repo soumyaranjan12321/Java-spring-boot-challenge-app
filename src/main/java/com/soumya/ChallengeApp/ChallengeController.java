b833332786531b520c379fde275d84e690c0cfd3
Soumyaranjan <soumya.ranjan12321@gmail.com>
2025-07-30 04:21:42 +0530

    Added a sping boot basic rest API app

---
+++ src/main/java/com/soumya/ChallengeApp/ChallengeController.java
@@ -0,0 +1,62 @@
+package com.soumya.ChallengeApp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/challenges")
+public class ChallengeController {
+    private ChallengeService challengeService;
+
+    public ChallengeController(ChallengeService challengeService) {
+        this.challengeService = challengeService;
+    }
+
+    @GetMapping
+    public List<Challenge> getAllChallenges() {
+        return challengeService.getAllChallenges();
+    }
+
+    @PostMapping
+    public String addChallenge(@RequestBody Challenge challenge) {
+        boolean isChallengeAdded = challengeService.addChallenge(challenge);
+        if (isChallengeAdded) {
+            return "Challenge added successfully";
+        } else {
+            return "Adding a challenge failed";
+        }
+    }
+
+    @GetMapping("/{month}")
+    public ResponseEntity<Object> getChallenge(@PathVariable String month) {
+        List<Challenge> matchingChallenges = challengeService.getChallenge(month);
+        if (!matchingChallenges.isEmpty()) {
+            return new ResponseEntity<>(matchingChallenges, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateChallenge(@PathVariable Long id, @RequestBody Challenge updatedChallenge) {
+        boolean isChallengeUpdated = challengeService.updateChallenge(id, updatedChallenge);
+        if (isChallengeUpdated) {
+            return new ResponseEntity<>("Challenge updated successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Updating a challenge failed", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteChallenge(@PathVariable Long id) {
+        boolean isChallengeDeleted = challengeService.deleteChallenge(id);
+        if (isChallengeDeleted) {
+            return new ResponseEntity<>("Challenge deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Deleting a challenge failed", HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/com/soumya/ChallengeApp/ChallengeService.java
@@ -0,0 +1,64 @@
+package com.soumya.ChallengeApp;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ChallengeService {
+    private List<Challenge> challenges = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public ChallengeService() {
+        //Challenge c1 = new Challenge(1L, "January", "Learn Java");
+        //challenges.add(c1);
+    }
+
+    public List<Challenge> getAllChallenges() {
+        return challenges;
+    }
+
+    public boolean addChallenge(Challenge challenge) {
+        if (challenge != null) {
+            challenge.setId(nextId++);
+            challenges.add(challenge);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public List<Challenge> getChallenge(String month) {
+        List<Challenge> matchingChallenges = new ArrayList<>();
+        for (Challenge challenge : challenges) {
+            if (challenge.getMonth().equalsIgnoreCase(month)) {
+                matchingChallenges.add(challenge);
+            }
+        }
+
+        return matchingChallenges;
+    }
+
+    public boolean updateChallenge(Long id, Challenge updatedChallenge) {
+        for (Challenge challenge : challenges) {
+            if (challenge.getId().equals(id)) {
+                challenge.setMonth(updatedChallenge.getMonth());
+                challenge.setDescription(updatedChallenge.getDescription());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteChallenge(Long id) {
+//        for (Challenge challenge : challenges) {
+//            if (challenge.getId().equals(id)) {
+//                challenges.remove(challenge);
+//                return true;
+//            }
+//        }
+//        return false;
+        return challenges.removeIf(challenge -> challenge.getId().equals(id));
+    }
+}
