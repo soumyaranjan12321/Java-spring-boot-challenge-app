4058e5e8d7e1e767f56e61706affa7bc671c1762
Soumyaranjan <soumya.ranjan12321@gmail.com>
2025-12-21 20:06:04 +0530

    Lombok integration

--- src/main/java/com/soumya/ChallengeApp/entity/ChallengeEntity.java
+++ src/main/java/com/soumya/ChallengeApp/entity/ChallengeEntity.java
@@ -1,12 +1,18 @@
 package com.soumya.ChallengeApp.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+/**
+ * ChallengeEntity - Represents a challenge entry in MongoDB
+ * Uses Lombok @Data annotation to auto-generate getters, setters, and toString
+ */
 @Document(collection = "challenge_entries")
+@Data
 public class ChallengeEntity {
     @Id
     private String id;
@@ -14,59 +20,27 @@ public class ChallengeEntity {
     private String description;
     private LocalDateTime date;
 
-    // No-arg constructor for MongoDB
+    /**
+     * No-arg constructor for MongoDB
+     * Automatically generates a UUID for the id field
+     */
     public ChallengeEntity() {
         this.id = UUID.randomUUID().toString();
     }
 
-    // Constructor with all fields
+    /**
+     * Constructor with all fields
+     *
+     * @param month the month of the challenge
+     * @param description the description of the challenge
+     * @param date the date of the challenge
+     */
     public ChallengeEntity(String month, String description, LocalDateTime date) {
         this.id = UUID.randomUUID().toString();
         this.month = month;
         this.description = description;
         this.date = date;
     }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getMonth() {
-        return month;
-    }
-
-    public void setMonth(String month) {
-        this.month = month;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    @Override
-    public String toString() {
-        return "ChallengeEntity{" +
-                "id='" + id + '\'' +
-                ", month='" + month + '\'' +
-                ", description='" + description + '\'' +
-                ", date=" + date +
-                '}';
-    }
 }
+
 
